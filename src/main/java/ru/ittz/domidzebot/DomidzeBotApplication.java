bf6726a65164b5d1ca334c63c885afab898a648e
snswd <sanswed@gmail.com>
2024-12-01 16:39:09 +0300

    checkstyle and welcome controller

--- src/main/java/ru/ittz/domidzebot/DomidzeBotApplication.java
+++ src/main/java/ru/ittz/domidzebot/DomidzeBotApplication.java
@@ -2,10 +2,29 @@ package ru.ittz.domidzebot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@SpringBootApplication
+import java.net.URI;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@RestController
 public class DomidzeBotApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(DomidzeBotApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DomidzeBotApplication.class, args);
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return ":: home page ::";
+    }
+    @GetMapping("/")
+    public ResponseEntity<Void> home() {
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .location(URI.create("/welcome"))
+                .build();
+    }
 }
--- src/test/java/ru/ittz/domidzebot/DomidzeBotApplicationTests.java
+++ src/test/java/ru/ittz/domidzebot/DomidzeBotApplicationTests.java
@@ -1,13 +1,7 @@
 package ru.ittz.domidzebot;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DomidzeBotApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
